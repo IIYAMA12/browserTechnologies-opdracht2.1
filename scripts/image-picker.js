eacd8b79ab878fa5cda9ee6eea8ff2cfe652f372
IIYAMA12 <jorik@134.167.028.145.hva.nl>
2018-03-21 09:53:18 +0100

    update content

--- scripts/image-picker.js
+++ scripts/image-picker.js
@@ -1,13 +1,17 @@
 (function (){
     var inputElement = document.getElementById("image-preview").getElementsByTagName("input")[0];
 
-    inputElement.removeAttribute("disabled");
-    
-    inputElement.addEventListener("change", function (e) {
-        var fileList = this.files;
+    // window.FileReader = null;
+    // delete window.FileReader;
 
-        if ("FileReader" in window) {
-            // https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+
+
+    if ("FileReader" in window) {
+
+        inputElement.removeAttribute("disabled");
+        inputElement.addEventListener("change", function (e) {
+            var fileList = this.files;
+        // https://developer.mozilla.org/en-US/docs/Web/API/FileReader
             var reader = new FileReader();
             reader.onload = (function(image) {
                 return function(e) {
@@ -16,8 +20,19 @@
             })(document.querySelector("#image-preview img"));
 
             reader.readAsDataURL(fileList[0]);
-        } else {
-            console.log("Ops this feature isn't available");
+        });
+        var feedbackElement = document.getElementById("image-preview").getElementsByTagName("p")[0];
+        if (feedbackElement != undefined) {
+            feedbackElement.parentElement.removeChild(feedbackElement);
         }
-    });
+    } else {
+        var feedbackElement = document.getElementById("image-preview").getElementsByTagName("p")[0];
+        if (feedbackElement != undefined) {
+            if ("textContent" in document) {
+                feedbackElement.textContent = "Upload feature unavailable";
+            } else {
+                feedbackElement.appendChild(document.createTextNode("Upload feature unavailable."));
+            }
+        }
+    }
 })();
